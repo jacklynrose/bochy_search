351dece5dbc7cbf0150c5c7d0228300783943f0e
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-29 15:19:05 +1100

    Basic filter algorithm and full name on model

--- app/assets/javascripts/collections/people.js
+++ app/assets/javascripts/collections/people.js
@@ -2,5 +2,12 @@ var App = App || {};
 
 App.People = Backbone.Collection.extend({
   url: "/people",
-  model: App.Person
+  model: App.Person,
+
+  filterBySearch: function(searchText) {
+    var matchedPeople = this.filter(function(person) {
+      return person.fullname() === searchText;
+    });
+    return new App.People(matchedPeople);
+  }
 });
--- app/assets/javascripts/models/person.js
+++ app/assets/javascripts/models/person.js
@@ -1,5 +1,9 @@
 var App = App || {};
 
 App.Person = Backbone.Model.extend({
-  urlRoot: "/people"
+  urlRoot: "/people",
+
+  fullname: function() {
+    return this.get('first_name') + " " + this.get('last_name');
+  }
 });
