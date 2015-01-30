0e8ae61f7099c565c4aef89c4030105e30fbd142
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-30 16:33:29 +1100

    Destroy and update

--- app/assets/javascripts/views/peopleView.js
+++ app/assets/javascripts/views/peopleView.js
@@ -3,7 +3,50 @@ var App = App || {};
 App.PeopleView = Backbone.View.extend({
 
   events: {
-    'keyup input': 'search'
+    'keyup .search_input': 'search',
+    // 'click li': 'fillSearch',
+    'click button': 'deletePerson',
+    'dblclick li' : 'renderForm', 
+    'submit form' : 'updatePerson'  
+  },
+
+  updatePerson: function(event) {
+    event.preventDefault();
+    var updatedFirst = $(event.currentTarget).find(".first_name").val();
+    var updatedLast = $(event.currentTarget).find(".last_name").val();
+    var id = $(event.currentTarget).parent().attr("data-id");
+    var person = this.collection.get(id);
+    person.save({
+      first_name: updatedFirst,
+      last_name: updatedLast
+    });
+    $(event.currentTarget).fadeOut();
+    this.search();
+  },
+
+  renderForm: function(event) {
+    var id = $(event.currentTarget).attr("data-id");
+    var person = this.collection.get(id);
+
+    $(event.currentTarget).html(JST['form'](
+      person.toJSON()
+    ));
+  },
+
+  deletePerson: function(event) {
+    var id = $(event.currentTarget).parent().attr("data-id");
+    event.stopPropagation();
+    $(event.currentTarget).parent().fadeOut();
+    this.collection.get(id).destroy();
+  },
+
+  fillSearch: function(event) {
+    var id = $(event.currentTarget).attr("data-id");
+
+    var textForFilling = this.collection.get(id).fullname();
+
+    this.$el.find("input").val(textForFilling);
+    this.search();
   },
 
   render: function() {
@@ -33,6 +76,25 @@ App.PeopleView = Backbone.View.extend({
         this.collection.filterBySearch(searchText).toJSON()
       );
     }
-  }
+  },
+
+  // createPerson: function() {
+  //   var firstName = this.$el.find(".first_name").val();
+  //   var lastName = this.$el.find(".last_name").val();
+
+  //   App.peopleCollection.create({
+  //     first_name: firstName,
+  //     last_name: lastName
+  //   });
+  // }
 
 });
+
+
+
+
+
+
+
+
+
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,27 @@ class PeopleController < ApplicationController
   def index
     render json: Person.all
   end
+
+  def create
+    @person = Person.create(person_params)
+    render json: @person
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    @person.update(person_params)
+    render json: @person
+  end
+
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+    render json: { status: "OK" }
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name)
+  end
 end
